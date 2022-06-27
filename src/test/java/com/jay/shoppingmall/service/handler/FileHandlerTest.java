1bf4a24d017fb07ed111ff607aa03e7c7ae7757f
4523454554 <choikj33@gmail.com>
2022-06-27 13:01:29 +0900

    FileHandlerTest InputStream에서 mocking byte[]로 변경.

--- src/test/java/com/jay/shoppingmall/service/handler/FileHandlerTest.java
+++ src/test/java/com/jay/shoppingmall/service/handler/FileHandlerTest.java
@@ -29,6 +29,7 @@ import java.util.Base64;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE_TIME;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class FileHandlerTest {
@@ -41,48 +42,47 @@ class FileHandlerTest {
     Image image;
 
     @Test
-    void whenUploadPngImage_ImageWillBeSaved_parseFilesInfo() throws IOException {
-        FileInputStream inputFile = new FileInputStream("C:\\Users\\choik\\Desktop\\QR코드.png");
-        MockMultipartFile file = new MockMultipartFile("file", "QR코드", "image/png", inputFile);
+    void whenUploadPngImage_ImageWillBeSaved_parseFilesInfo() {
+        byte[] content = new byte['q'];
+
+        MockMultipartFile file = new MockMultipartFile("file", "원본파일이름", "image/png", content);
+
         image = fileHandler.parseFilesInfo(file, ImageRelation.ITEM_MAIN, 0L);
 
         assertThat(image.getFileExtension()).isEqualTo(".png");
-        assertThat(image.getOriginalFileName()).isEqualTo("QR코드");
+        assertThat(image.getOriginalFileName()).isEqualTo("원본파일이름");
         assertThat(image.getForeignId()).isEqualTo(0L);
         assertThat(image.getFileSize()).isNotNull();
         assertThat(image.getFilePath()).contains("images");
         assertThat(image.getFilePath()).contains(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
 
-        inputFile.close();
         fileHandler.deleteImage(image);
     }
 
     @Test
     void whenUploadJpegImage_ImageWillBeSaved_parseFilesInfo() throws IOException {
+        byte[] content = new byte['q'];
 
-        FileInputStream inputFile = new FileInputStream("C:\\Users\\choik\\Desktop\\QR코드.jpg");
-        MockMultipartFile file = new MockMultipartFile("file", "QR코드", "image/jpeg", inputFile);
+        MockMultipartFile file = new MockMultipartFile("file", "원본파일이름", "image/jpeg", content);
         image = fileHandler.parseFilesInfo(file, ImageRelation.ITEM_MAIN, 0L);
 
         assertThat(image.getFileExtension()).isEqualTo(".jpg");
-        assertThat(image.getOriginalFileName()).isEqualTo("QR코드");
+        assertThat(image.getOriginalFileName()).isEqualTo("원본파일이름");
         assertThat(image.getForeignId()).isEqualTo(0L);
         assertThat(image.getFileSize()).isNotNull();
         assertThat(image.getFilePath()).contains("images");
         assertThat(image.getFilePath()).contains(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
 
-        inputFile.close();
         fileHandler.deleteImage(image);
     }
 
     @Test
     void whenUploadNotJpegOrPng_ThrowFileException_parseFilesInfo() {
         try {
-            FileInputStream inputFile = new FileInputStream("C:\\Users\\choik\\Desktop\\QR코드");
-            MockMultipartFile file = new MockMultipartFile("file", "QR코드", MediaType.TEXT_PLAIN_VALUE, inputFile);
+            byte[] content = new byte['q'];
+            MockMultipartFile file = new MockMultipartFile("file", "원본파일이름", MediaType.TEXT_PLAIN_VALUE, content);
 
             assertThrows(FileException.class, () -> fileHandler.parseFilesInfo(file, ImageRelation.ITEM_MAIN, 0L));
-            inputFile.close();
         } catch (Exception e) {
             e.getStackTrace();
         }
@@ -90,10 +90,9 @@ class FileHandlerTest {
 
     @Test
     void getStringImage() throws IOException {
-        FileInputStream inputFile = new FileInputStream("C:\\Users\\choik\\Desktop\\QR코드.png");
-        MockMultipartFile file = new MockMultipartFile("file", "QR코드", MediaType.IMAGE_PNG_VALUE, inputFile);
+        byte[] content = new byte['q'];
+        MockMultipartFile file = new MockMultipartFile("file", "원본파일이름", MediaType.IMAGE_PNG_VALUE, content);
         image = fileHandler.parseFilesInfo(file, ImageRelation.ITEM_MAIN, 0L);
-        inputFile.close();
 
         final String stringImage = fileHandler.getStringImage(image);
         final boolean isBase64Encoded = stringImage.matches("^[a-zA-Z0-9+/]*={0,2}$");
